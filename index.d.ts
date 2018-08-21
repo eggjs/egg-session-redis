b0c9f07760bc9d5d96950cd2dbfff2cfd2700cae
songyazhao <syazhao@foxmail.com>
2018-08-21 19:29:11 +0800

    [New Feature] add typescript support

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module 'egg' {
+  export interface EggAppConfig {
+    sessionRedis: {
+      name: string
+    }
+  }
+
+  export interface Application {j
+    /**
+     * @member Application#sessionStore
+     * @property {Function<string>} get - get redis session store
+     * @property {Function<string, any, number?>} set - set the redis session store
+     * @property {Function<string>} destroy - destroy of redis session store
+     * @example
+     * ```js
+     * this.app.sessionStore.set('SESSION_KEY', { a: 1 }, 6000);
+     * this.app.sessionStore.get('SESSION_KEY');
+     * this.app.sessionStore.destroy('SESSION_KEY');
+     * ```
+     */
+    sessionStore: {
+      get: (key: string) => Promise<any>;
+      set: (key: string, value: any, maxAge?: number) => Promise<void>;
+      destroy: (key: string) => Promise<void>;
+    };
+  }
+}
